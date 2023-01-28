cf8e0de584ecd78ebc39c009be87042416efe9d1
Cam-Bloom <cam.w.bloomfield@gmail.com>
2023-01-28 16:33:30 +0000

    navbar show hide complete

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Portfolio from './components/Portfolio'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Navbar></Navbar>
       <About></About>
+      <Portfolio></Portfolio>
     </div>
   )
 }
--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -2,13 +2,29 @@ import {FiHome} from 'react-icons/fi'
 import {FiUser} from 'react-icons/fi'
 import {FiBook} from 'react-icons/fi'
 import {FiMessageCircle} from 'react-icons/fi'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import useScrollListener from '../hooks/useScrollListener'
 
 const Navbar = () => {
-    const [activeNav, setActiveNav] = useState('#home')
+    const [activeNav, setActiveNav] = useState()
+    const [navClassList, setNavClassList] = useState(["hidden"]);
+    const scroll = useScrollListener();
 
+    useEffect(() => {
+        const classList = ['hidden'];
+
+        if (scroll.y > 120 ){
+            classList.pop()
+            classList.push("active");
+        }
+
+        setNavClassList(classList);
+
+    }, [scroll.y, scroll.lastY])
+
+    console.log(navClassList);
   return (
-    <nav>
+    <nav className={navClassList.join(" ")}>
         <a href= "#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home'  ? 'active': ''}><FiHome></FiHome></a>
         <a href= "#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about'  ? 'active': ''}><FiUser></FiUser></a>
         <a href= "#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio'  ? 'active': ''}><FiBook></FiBook></a>
