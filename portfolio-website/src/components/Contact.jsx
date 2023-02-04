80c09d861516594731d349d0edb9a7ec02d27b57
Cam-Bloom <cam.w.bloomfield@gmail.com>
2023-02-04 11:41:38 +0000

    V1 contact form completed working with email connection

--- portfolio-website/src/components/Contact.jsx
+++ portfolio-website/src/components/Contact.jsx
@@ -1,7 +1,44 @@
-const Contact = () => {
-  return (
-    <section>Contact</section>
-  )
-}
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-export default Contact
+const Contact = () => {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm("service_gmc33c7", "template_5ytncv8", form.current, "vZu02FFUw9kSiSzri").then(
+			(result) => {
+				console.log(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			}
+		);
+
+		e.target.reset();
+	};
+
+	return (
+		<section id="contact" className="container">
+			<h5>Get In Touch</h5>
+			<h2>Contact Me</h2>
+
+			<form
+				ref={form}
+				onSubmit={(e) => {
+					sendEmail(e);
+				}}
+			>
+				<input type="text" name="name" placeholder="Your Full Name" required />
+				<input type="email" name="email" placeholder="Your Email" required />
+				<textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+				<button type="submit" className="btn btn-primary">
+					Send Message
+				</button>
+			</form>
+		</section>
+	);
+};
+
+export default Contact;
