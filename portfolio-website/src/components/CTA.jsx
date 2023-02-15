cf6d9d93588a7bb3bcd5bc27ed4c919ebb1bf708
Cam-Bloom <cam.w.bloomfield@gmail.com>
2023-02-15 07:23:26 +0000

    CV link fix for production

--- portfolio-website/src/components/CTA.jsx
+++ portfolio-website/src/components/CTA.jsx
@@ -2,7 +2,7 @@
 const CTA = () => {
 	return (
 		<div className="cta">
-			<a className="btn" href='/src/assets/CV-Cameron-Bloomfield.pdf' download >
+			<a className="btn" href='https://drive.google.com/uc?export=download&id=1S-vcOdt2rL0P2zylFu19nxz6ZsypqDyl' download >
 				Download CV
 			</a>
 			<a className="btn btn-primary" href="#portfolio">
